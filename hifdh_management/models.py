8f052eccb3317915b60677857f01b2d5be30d275
Anonymity1234 <latazlimnafsak@gmail.com>
2023-05-29 13:07:59 -0400

    initial setup

---
+++ hifdh_management/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+import json
+
+from django.core.serializers.json import DjangoJSONEncoder
+# Create your models here.
+
+class Hifdh_Table(models.Model):
+
+    teacher_name_choices = [('Israr', 'Israr'), ('Qari Abdul Azeem', 'Qari Abdul Azeem')]
+
+    name = models.CharField(max_length=500)
+    surah = models.CharField(max_length=500)
+    ayah_start = models.IntegerField()
+    ayah_end = models.IntegerField()
+    mistakes = models.IntegerField()
+    stuck = models.IntegerField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    teacher = models.CharField(max_length=500, choices=teacher_name_choices, default='Qari Abdul Azeem')
